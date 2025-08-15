c986c51fe599a3efe295c3dadf62b6c4b4d86c2d
Ayoub101010 <ayoub.dahdouh@etu.uae.ac.ma>
2025-08-15 16:09:45 +0100

    L'ajout de l'api point

--- API_GeoDjango/pprcollecte/api/serializers.py
+++ API_GeoDjango/pprcollecte/api/serializers.py
@@ -2,6 +2,85 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from .models import Login
 from .models import Piste
+from .models import (
+    ServicesSantes, AutresInfrastructures, Bacs, BatimentsAdministratifs,
+    Buses, Dalots, Ecoles, InfrastructuresHydrauliques, Localites,
+    Marches, PassagesSubmersibles, Ponts
+)
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+
+class ServicesSantesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = ServicesSantes
+        geo_field = "geom"
+        fields = '__all__'
+
+class AutresInfrastructuresSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = AutresInfrastructures
+        geo_field = "geom"
+        fields = '__all__'
+
+class BacsSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Bacs
+        geo_field = "geom"
+        fields = '__all__'
+
+class BatimentsAdministratifsSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = BatimentsAdministratifs
+        geo_field = "geom"
+        fields = '__all__'
+
+class BusesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Buses
+        geo_field = "geom"
+        fields = '__all__'
+
+class DalotsSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Dalots
+        geo_field = "geom"
+        fields = '__all__'
+
+class EcolesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Ecoles
+        geo_field = "geom"
+        fields = '__all__'
+
+class InfrastructuresHydrauliquesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = InfrastructuresHydrauliques
+        geo_field = "geom"
+        fields = '__all__'
+
+class LocalitesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Localites
+        geo_field = "geom"
+        fields = '__all__'
+
+class MarchesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Marches
+        geo_field = "geom"
+        fields = '__all__'
+
+class PassagesSubmersiblesSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = PassagesSubmersibles
+        geo_field = "geom"
+        fields = '__all__'
+
+class PontsSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Ponts
+        geo_field = "geom"
+        fields = '__all__'
+
 
 class LoginSerializer(serializers.ModelSerializer):
     class Meta:
--- API_GeoDjango/pprcollecte/api/urls.py
+++ API_GeoDjango/pprcollecte/api/urls.py
@@ -1,8 +1,27 @@
 from django.urls import path
 from api.views import LoginAPIView  # import absolu recommandé
 from .views import PisteListCreateAPIView
+from .views import (
+    LoginAPIView, PisteListCreateAPIView,
+    ServicesSantesListCreateAPIView, AutresInfrastructuresListCreateAPIView, BacsListCreateAPIView,
+    BatimentsAdministratifsListCreateAPIView, BusesListCreateAPIView, DalotsListCreateAPIView,
+    EcolesListCreateAPIView, InfrastructuresHydrauliquesListCreateAPIView, LocalitesListCreateAPIView,
+    MarchesListCreateAPIView, PassagesSubmersiblesListCreateAPIView, PontsListCreateAPIView
+)
 
 urlpatterns = [
     path('api/login/', LoginAPIView.as_view(), name='api-login'),
-     path('api/pistes/', PisteListCreateAPIView.as_view(), name='api-pistes'),
+    path('api/pistes/', PisteListCreateAPIView.as_view(), name='api-pistes'),
+    path('api/services_santes/', ServicesSantesListCreateAPIView.as_view(), name='api-services-santes'),
+    path('api/autres_infrastructures/', AutresInfrastructuresListCreateAPIView.as_view(), name='api-autres-infrastructures'),
+    path('api/bacs/', BacsListCreateAPIView.as_view(), name='api-bacs'),
+    path('api/batiments_administratifs/', BatimentsAdministratifsListCreateAPIView.as_view(), name='api-batiments-administratifs'),
+    path('api/buses/', BusesListCreateAPIView.as_view(), name='api-buses'),
+    path('api/dalots/', DalotsListCreateAPIView.as_view(), name='api-dalots'),
+    path('api/ecoles/', EcolesListCreateAPIView.as_view(), name='api-ecoles'),
+    path('api/infrastructures_hydrauliques/', InfrastructuresHydrauliquesListCreateAPIView.as_view(), name='api-infrastructures-hydrauliques'),
+    path('api/localites/', LocalitesListCreateAPIView.as_view(), name='api-localites'),
+    path('api/marches/', MarchesListCreateAPIView.as_view(), name='api-marches'),
+    path('api/passages_submersibles/', PassagesSubmersiblesListCreateAPIView.as_view(), name='api-passages-submersibles'),
+    path('api/ponts/', PontsListCreateAPIView.as_view(), name='api-ponts'),
 ]
